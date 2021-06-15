9cbec4581748adad7849efdcb63023a9dc85a233
Matthew Collier <mcollier@digitalbazaar.com>
2021-06-15 17:42:59 -0400

    Setup linting.

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    node: true
+  },
+  extends: ['digitalbazaar'],
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020
+  }
+};
--- helloworld.js
+++ helloworld.js
@@ -1,11 +1,13 @@
-var bedrock = require('bedrock');
+const bedrock = require('bedrock');
 
 // includes the bedrock-express module which provides express capabilities.
 require('bedrock-express');
 
-// Waits for express to being listening to routes, which then allows us to provide our own handlers for provided routes
+// Waits for express to being listening to routes, which then allows us to
+// provide our own handlers for provided routes
 bedrock.events.on('bedrock-express.configure.routes', function(app) {
-  // listens to routes on the server at the root directory and responds with "Hello World!".
+  // listens to routes on the server at the root directory and responds
+  // with "Hello World!".
   app.get('/', function(req, res) {
     res.send('Hello World!');
   });
