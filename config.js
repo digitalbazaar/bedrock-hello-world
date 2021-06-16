f9e97028b535b4468da636c9ecd881290168ee3d
Matthew Collier <mcollier@digitalbazaar.com>
2021-06-15 21:17:33 -0400

    Update copyright notices.

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) 2015-2021 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
 const {config} = require('bedrock');
 
 config['hello-world'] = {
--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) 2015-2021 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
 const bedrock = require('bedrock');
 const {config} = bedrock;
 
